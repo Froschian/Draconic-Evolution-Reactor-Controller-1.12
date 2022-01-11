b644904e753eaf00aa8bf779265b149e029eb40f
Froschian <92104129+Froschian@users.noreply.github.com>
2022-01-11 20:37:29 +0100

    bug fixed and cleaned up

--- install.lua
+++ install.lua
@@ -2,7 +2,7 @@
 --rhNbNbFE
 --Version 1.0
 
-local libURL = "https://raw.githubusercontent.com/Froschian/Draconic-Evolution-Reactor-Controller-1.12/main/lib/f.lua"
+local libURL = "https://raw.githubusercontent.com/Froschian/Draconic-Evolution-Reactor-Controller-1.12/main/lib/lib.lua"
 local startupURL = "https://raw.githubusercontent.com/Froschian/Draconic-Evolution-Reactor-Controller-1.12/main/Draconic-Evolution-Reactor-Controller.lua"
 local lib, startup
 local libFile, startupFile
@@ -12,7 +12,7 @@ fs.makeDir("lib")
 lib = http.get(libURL)
 libFile = lib.readAll()
 
-local file1 = fs.open("lib/f", "w")
+local file1 = fs.open("lib/lib", "w")
 file1.write(libFile)
 file1.close()
 
